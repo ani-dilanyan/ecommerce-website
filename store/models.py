f328d0a358b3e1c0546c5bd262c8433f1f2f2477
ani-dilanyan <anidilanyan05@gmail.com>
2023-10-15 16:17:16 +0400

    Add ImageField to Product model for main image and add corresponding functionalities

--- store/models.py
+++ store/models.py
@@ -14,11 +14,18 @@ class Customer(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.FloatField()
-    #image
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self) -> str:
         return self.name
     
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = 'images/placeholder.png'
+        return url
 
 class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
--- store/views.py
+++ store/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def store(request):
-    context = {}
+    products = Product.objects.all()
+    context = {'products': products}
     return render(request, 'store/store.html', context)
 
 def cart(request):
@@ -12,3 +14,6 @@ def cart(request):
 def checkout(request):
     context = {}
     return render(request, 'store/checkout.html', context)
+
+
+# 
